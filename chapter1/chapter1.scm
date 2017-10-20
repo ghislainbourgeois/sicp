428d73c0ef3ac8dbde592106c66059a762fb2e5c
Ghislain Bourgeois <ghislain.bourgeois@gmail.com>
2017-10-19 22:15:12 -0400

    Reformat comments to double ';'

--- chapter1/chapter1.scm
+++ chapter1/chapter1.scm
@@ -1,26 +1,26 @@
-;                     Exercise 1.1
-;
-; 10
-; 12
-; 8
-; 3
-; 6
-; a
-; b
-; 19
-; #f
-; 4
-; 16
-; 6
-; 16
+;;                    Exercise 1.1
+;;
+;; 10
+;; 12
+;; 8
+;; 3
+;; 6
+;; a
+;; b
+;; 19
+;; #f
+;; 4
+;; 16
+;; 6
+;; 16
 
 
-;                     Exercise 1.2
+;;                    Exercise 1.2
 (/ (+ 5 4 (- 2 (- 3 (+ 6 (/ 4 5)))))
    (* 3 (- 6 2) (- 2 7)))
 
 
-;                     Exercise 1.3
+;;                    Exercise 1.3
 (define (square x)
         (* x x))
 
@@ -34,42 +34,42 @@
               (else (sum-of-squares a b))))
 
 
-;                     Exercise 1.4
-;
-; This defines a procedure named a-plus-abs-b that takes two arguments, a and b.
-; When executed, the special form 'if' will first make the interpreter evaluate the
-; condition (> b 0). If the condition is true, the 'if' will evaluate to '+', else it
-; will evaluate to '-'. It will then add or substract b from a. This will effectively
-; be the same thing as adding the absolute value of b to a.
+;;                    Exercise 1.4
+;;
+;; This defines a procedure named a-plus-abs-b that takes two arguments, a and b.
+;; When executed, the special form 'if' will first make the interpreter evaluate the
+;; condition (> b 0). If the condition is true, the 'if' will evaluate to '+', else it
+;; will evaluate to '-'. It will then add or substract b from a. This will effectively
+;; be the same thing as adding the absolute value of b to a.
 
 
-;                     Exercise 1.5
-;
-; With applicative-order evaluation, (p) will first be evaluated. It will evaluate
-; to itself in a recursive fashion, and the procedure will not terminate.
-;
-; With the normal-order evaluation, the following evaluation will take place:
-;
-; (test 0 (p))
-; (if (= 0 0) 0 (p))
-; (if #t 0 (p))
-; 0
-;
-; In this case, (p) will never be evaluated so the procedure terminates with a value
-; of 0.
+;;                    Exercise 1.5
+;;
+;; With applicative-order evaluation, (p) will first be evaluated. It will evaluate
+;; to itself in a recursive fashion, and the procedure will not terminate.
+;;
+;; With the normal-order evaluation, the following evaluation will take place:
+;;
+;; (test 0 (p))
+;; (if (= 0 0) 0 (p))
+;; (if #t 0 (p))
+;; 0
+;;
+;; In this case, (p) will never be evaluated so the procedure terminates with a value
+;; of 0.
 
 
-;                     Exercise 1.6
-;
-; The procedure will never terminate and eventually crash. The reason is that new-if
-; is not a special form, so applicative-order evaluation will be used. This means that
-; the evaluation of the else-clause will be evaluated before new-if, creating an
-; infinite recursive call.
+;;                    Exercise 1.6
+;;
+;; The procedure will never terminate and eventually crash. The reason is that new-if
+;; is not a special form, so applicative-order evaluation will be used. This means that
+;; the evaluation of the else-clause will be evaluated before new-if, creating an
+;; infinite recursive call.
 
 
-;                     Exercise 1.7
+;;                    Exercise 1.7
 
-; Original version of sqrt-iter
+;; Original version of sqrt-iter
 (define (sqrt-iter guess x)
   (if (good-enough? guess x)
     guess
@@ -81,25 +81,25 @@
 (define (average x y)
   (/ (+ x y) 2))
 
-; Original version good-enough?
+;; Original version good-enough?
 (define (good-enough? guess x)
   (< (abs (- (square guess) x)) 0.001))
 
 (define (sqrt x)
   (sqrt-iter 1 x))
 
-; Improved version of good-enough?
+;; Improved version of good-enough?
 (define (good-enough? guess improved)
   (< (/ (abs (- guess improved)) guess) 0.0001))
 
-; Improved version of sqrt-iter
+;; Improved version of sqrt-iter
 (define (sqrt-iter guess x)
   (if (good-enough? guess (improve guess x))
     guess
     (sqrt-iter (improve guess x) x)))
 
 
-;                     Exercise 1.8
+;;                    Exercise 1.8
 
 (define (cube-root-iter guess x)
   (if (good-enough? guess (improve-cube-root guess x))
@@ -113,7 +113,7 @@
   (cube-root-iter 1 x))
 
 
-; Exercise 1.9
+;;                    Exercise 1.9
 ;;
 ;; (+ 4 5)
 ;; (if (= 4 0) 5 (inc (+ (dec 4) 5)))
